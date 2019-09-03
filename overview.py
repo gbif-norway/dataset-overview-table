5696e9e9ee5789d3dbfb33fd64578bbc3925530c
rukayaj <rukayaj@gmail.com>
2019-09-03 14:37:52 +0200

    Update with new script

--- overview.py
+++ overview.py
@@ -4,6 +4,8 @@ import sys
 import json
 import pystache
 import requests
+from dateutil import parser as dateparser
+import dateutil
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -19,26 +21,29 @@ query = { 'publishingCountry': 'NO', 'limit': 500 }
 response = requests.get(GBIF + "dataset/search", params=query).json()
 
 datasets = []
-installations = {}
+sources = {}
 for result in response['results']:
     data = requests.get(GBIF + "dataset/" + result['key']).json()
-    ikey = data['installationKey']
-    if not ikey in installations:
-        installations[ikey] = requests.get(GBIF + "installation/" + ikey).json()
+    source = data['installationKey']
+    if not source in sources:
+        sources[source] = requests.get(GBIF + "installation/" + source).json()
+
+    # doesn't look like we can trust the record counts from the dataset search
     occurrences = requests.get(GBIF + "occurrence/search",
             params={ 'datasetKey': result['key'] }).json()
     datasets.append({
         'key': result['key'],
         'title': result['title'],
         'occurrenceCount': occurrences['count'],
-        'installation': installations[ikey]['title'],
+        'installation': sources[source]['title'],
         'organization': result['publishingOrganizationTitle'],
-        'type': result['type'].capitalize()
+        'type': result['type'].replace("_", " ").capitalize(),
+        'created': dateparser.parse(data['created']).strftime("%Y-%m-%d"),
+        'modified': dateparser.parse(data['modified']).strftime("%Y-%m-%d"),
     })
 
-
-datasets = sorted(datasets, key=lambda k: (k['type'], k['title']))
 template = open("template.html", "r").read()
+datasets = sorted(datasets, key=lambda k: (k['type'], k['title']))
 
 renderer = pystache.Renderer()
 html = renderer.render(template, { 'datasets': datasets })
